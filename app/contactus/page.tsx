5eac4482437dc890e72868a80ecbc5c8b31ce8b9
AbdulMuneebSyed-2ndAccount <sadmuneeb786@gmail.com>
2024-12-31 02:04:23 +0530

    fixed

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -2,10 +2,7 @@ import React from 'react'
 import Contactus from "@/components/contactus"
 import Navbar2 from "@/components/navbar2";
 import Footer from "@/components/Footer";
-import OrbitingCirclesDemo from "@/app/new/page";
-import About from "@/components/about";
-import LandServices from "@/components/LandServices";
-import Navbar from "@/components/navbar";
+
 
 import Hero from "@/components/hero";
 const page = () => {
