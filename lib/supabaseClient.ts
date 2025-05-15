9f0d9ab7ed22c0133f27c51b12dadf54792c61cd
capcocs4githu <capcocsqa@gmail.com>
2025-05-15 18:49:44 +0530

    Update 2.0

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://brfbhphjfbqfbywcumsv.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyZmJocGhqZmJxZmJ5d2N1bXN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3ODIxNDYsImV4cCI6MjA2MjM1ODE0Nn0.xwP6qmOsCnFBvWgNdcg1qed9v7PVf5oSm3ueYuf7JMA";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
